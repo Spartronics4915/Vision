99865c3516793b8c0d249e314190565fae745a28
loqoman <darwin.s.clark@gmail.com>
2019-02-28 11:11:02 -0800

    Move all methods from pnpsorting to rectutil

--- 2019/rectUtil.py
+++ 2019/rectUtil.py
@@ -255,6 +255,57 @@ def checkCenter(point,currentCenter):
     else:
         return False
 
+def sortPoints(leftPoints, rightPoints):
+    # XXX: change defualt debug value
+    logging.debug("Received a leftPoints of: " + str(leftPoints))
+    logging.debug("Received a rightPoints of:" + str(rightPoints))
+    logging.debug("Type of the points is: " + str(type(leftPoints[0])))
+    logging.debug("A sample point is: " + str(leftPoints[0]))
+    logging.debug("A sample value is: " + str(type(leftPoints[0][0])))
+    """
+    Doctest logic
+    >>> sortPoints([(2,1),(2,4),(1,3),(3,2)],[(7,1),(8,3),(6,2),(7,4)])
+    [(2, 1), (3, 2), (2, 4), (6, 2), (7, 1), (7, 4)]
+    """
+    orderedPoints = []
+    leftSorted = sorted(leftPoints,key=lambda p:p[1])
+    rightSorted = sorted(rightPoints,key=lambda p:p[1])
+    # Now we have a list of points, sorted by y
+    topLPair = [leftSorted[0],leftSorted[1]]
+    botLPair = [leftSorted[2],leftSorted[3]]
+    # We now have the L points clumped by pair
+    topRPair = [rightSorted[0],rightSorted[1]]
+    botRPair = [rightSorted[2],rightSorted[3]]
+    # We now have the R points clumped by pair
+
+    logging.debug("Sorting points by x, with the leftmost first")
+
+    # Sorting by x, with smallest x first
+    topLPair = sorted(topLPair,key=lambda p:p[0])
+    botLPair = sorted(botLPair,key=lambda p:p[0])
+
+    topRPair = sorted(topRPair,key=lambda p:p[0])
+    botRPair = sorted(botRPair,key=lambda p:p[0])
+
+    # Appending proper points
+    ### LEFT RECT ###
+    # Top-left 
+    orderedPoints.append(topLPair[0])
+    # Top-right
+    orderedPoints.append(topLPair[1])
+    # Bot-right
+    orderedPoints.append(botLPair[1])
+    ### RIGHT RECT ###
+    # Top-left
+    orderedPoints.append(topRPair[0])
+    # Top-right
+    orderedPoints.append(topRPair[1])
+    # Bot-left
+    orderedPoints.append(botRPair[0])
+
+    return np.array(orderedPoints,dtype="double")
+
+
 if __name__ == "__main__":
     import doctest
 
