884162bcae8e1c9af9182ebee2a08243e508b4ce
Jon Q Programmer <Spartronics4915@gmail.com>
2019-03-15 19:58:36 +0000

    need to make writeable log dir on each reboot

--- 2019/startRWS.sh
+++ 2019/startRWS.sh
@@ -1,10 +1,25 @@
 #!/bin/bash -f
 
+logdir=/var/tmp/rmslog
+if [ ! -d $logdir ]; then
+  mkdir -p $logdir
+  mkdir $logdir/00
+  mkdir $logdir/01
+  mkdir $logdir/02
+  mkdir $logdir/03
+  mkdir $logdir/04
+  mkdir $logdir/05
+  mkdir $logdir/06
+  mkdir $logdir/07
+  mkdir $logdir/08
+  mkdir $logdir/09
+fi
+
 webhome=/home/pi/spartronics/Vision/2019/rwsweb
 cd /opt/rws
 ./webrtc-streamer \
   -conf "$webhome/etc/webrtc_streamer.conf" \
-  -log /var/log/rws
+  -log $logdir
 
 echo "rws terminated"
 
