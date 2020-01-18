2551a3626d5f57634be1fd08ee2ab1c324eceb0a
loqoman <darwin.s.clark@gmail.com>
2020-01-18 13:37:02 -0800

    Mirror to rectUtil.py | Perhaps have pnp vals in it?

---
+++ 2020/targetUtils.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+# Sepreate file to manage all mid-low level functions managing the targets
+# Ideally, ALL functions accept a config, frame, and variables that are specific
+# to that pipeline
+
+# For short-ness purposes:
+#       target = half-hexagon
+
+# TODO: Integration Tests
+# TODO: Take 
+import numpy as np
+import cv2
+import logging
+
+def findTarget(frame, cfg):
+    pass
+
+def estimatePosePNP(frame, cfg):
+    pass
+
+def getTargetCenter(frame, cfg):
+    pass
