f9bb508719ecaace7a69fe3adb1178d6485df818
Marshall Tappen <mtappen@gmail.com>
2020-02-07 16:19:20 -0800

    Added a testLatency program for testing how long it takes the pi to run solvePNP

---
+++ 2020/testLatency.py
@@ -0,0 +1,30 @@
+'''
+Tests a wide variety of frames to find the average time it takes to solvePNP
+'''
+from pathlib import Path
+
+import numpy as np
+import algo
+import config
+import time
+import cv2
+
+cfg = config.moduleDebuggingConfig['algo']
+top_dir = Path('data/1080p_test')
+img_dir = [test_dir for test_dir in top_dir.glob('test_imgs_*')]
+computationTimes = []
+imgsProcessed = 0
+
+for test_dir in img_dir:
+    fnames = [str(f) for f in test_dir.glob('*.png')]
+
+    for f in fnames:
+        frame = cv2.imread(f)
+        initTime = time.monotonic()
+        _, _ = algo.realPNP(frame,cfg)
+        finalTime = time.monotonic()
+        computationTimes.append(finalTime-initTime)
+        imgsProcessed += 1 
+
+print("Total processing time: {}".format(sum(computationTimes)))
+print("/n Average processing time: {}".format(sum(computationTimes)/imgsProcessed))
