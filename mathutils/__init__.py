47e45f7c3ba07c559f3651dbb368139de7afb7ae
Dana Batali <dana.batali@gmail.com>
2020-02-12 14:25:13 -0800

    Minor unimportant nits.

--- mathutils/__init__.py
+++ mathutils/__init__.py
@@ -1,3 +1,7 @@
+# Learn about modules and packages here:
+#    https://alex.dzyoba.com/blog/python-import/
+# and here: 
+#    https://docs.python.org/3/tutorial/modules.html
 from mathutils.affine import *
 from mathutils.vec import *
 from mathutils.camToField2020 import *
--- mathutils/vec.py
+++ mathutils/vec.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 """ vec.py contains these 'classes' (actually more like namespaces)
         Vec3
@@ -7,7 +8,7 @@ import numpy as np
 
 class Vec3(object):
     """
-    A helper class for 3d points, just to hide obscure numpy syntax
+    A helper class for 3d points, just to hide/centralize numpy syntax
 
     >>> vec = [1,2,3]
     >>> len = Vec3.length(vec)
@@ -32,10 +33,22 @@ class Vec3(object):
         """ nb: this wont overwrite vec3 unless it's an np.array """
         len = np.linalg.norm(vec3, 2, -1)
         return vec3/len
+    
+    @staticmethod
+    def dot(a, b):
+        return np.dot(a, b)
+    
+    @staticmethod
+    def angleBetween(a, b):
+        au = Vec3.normalize(a)
+        bu = Vec3.normalize(b)
+        dot = np.dot(au, bu)
+        return math.degrees(math.acos(dot))
+
 
 class Vec2(object):
     """
-    A helper class for 2d points, just to hide obscure numpy syntax
+    A helper class for 2d points, just to hide/centralize numpy syntax
 
     >>> vec = [1,1]
     >>> len = Vec2.length(vec)
