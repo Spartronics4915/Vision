012203beb51611da12c0eed2ad2bc5b556995291
Randy Groves <randomgrace@gmail.com>
2022-03-08 15:06:45 -0800

    Sync up scripts with actual RPi scripts.

---
+++ tools/DriverCamScripts/RPi/keep_gstream.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+target=${1:-10.49.15.20}
+port=${2:-5805}
+
+while true
+do
+	check=`/home/pi/check_gstream.sh`
+	if [ -z "$check" ]; then
+		now=`date`
+		echo "Restarting $now"
+		/home/pi/gstreamit.sh $target $port > /dev/null 2>&1 &
+	else
+		sleep 30
+	fi
+done
--- tools/DriverCamScripts/RPi/kill_gstream.sh
+++ tools/DriverCamScripts/RPi/kill_gstream.sh
@@ -3,3 +3,5 @@
 
 export killit=`ps -ef | grep gst-launch | grep -v grep | awk '{print $2}'`
 kill -9 $killit
+export killit2=`ps -ef | grep gstreamit.sh | grep -v grep | awk '{print $2}'`
+kill -9 $killit2
