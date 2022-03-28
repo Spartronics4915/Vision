de988a3c86224f3a927d976555d91fcfef42cc56
Randy Groves <randomgrace@gmail.com>
2022-03-28 12:11:28 -0700

    Change default driver station IP
    Need to set something in the 5-20 range.

--- tools/DriverCamScripts/Windows/SetEther7.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if "%1" == "dhcp" (
-	netsh interface ipv4 set address name="Ethernet 7" dhcp
-	netsh interface ipv4 set dnsservers name="Ethernet 7" dhcp validate=no
-) else (
-	netsh interface ipv4 set address name="Ethernet 7" static 10.49.15.20 255.0.0.0 10.49.15.1
-	netsh interface ipv4 set dns name="Ethernet 7" static 10.49.15.1 validate=no
-)
-pause
--- tools/DriverCamScripts/Windows/SetWifi.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if "%1" == "dhcp" (
-	netsh interface ipv4 set address name="Wi-Fi" dhcp
-	netsh interface ipv4 set dnsservers name="Wi-Fi" dhcp validate=no
-) else (
-	netsh interface ipv4 set address name="Wi-Fi" static 10.49.15.20 255.0.0.0 10.49.15.1
-	netsh interface ipv4 set dns name="Wi-Fi" static 10.49.15.1 validate=no
-)
-pause
